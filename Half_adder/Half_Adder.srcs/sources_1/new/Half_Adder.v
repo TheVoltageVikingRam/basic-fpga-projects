46048eadc732b6d99e4c0a12df4f22ff06d99d1e
Ram Tripathi <ram0183rt@gmail.com>
2025-08-05 13:38:44 +0530

    update

---
+++ Half_adder/Half_Adder.srcs/sources_1/new/Half_Adder.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/05/2025 01:36:18 PM
+// Design Name: 
+// Module Name: Half_Adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Half_Adder(
+
+    );
+endmodule
